237b7301d7e094afd71ad9bb98264016f4bfa644
StanislavPB <perminovsb@gmail.com>
2022-12-21 23:20:12 +0200

    feat: add lesson 6

---
+++ src/main/java/lessons/lesson6/Pair.java
@@ -0,0 +1,15 @@
+package lessons.lesson6;
+
+public class Pair {
+    int age;
+
+    String name;
+
+    double weigth;
+
+    public Pair(int age, String name, double weigth) {
+        this.age = age;
+        this.name = name;
+        this.weigth = weigth;
+    }
+}
---
+++ src/main/java/lessons/lesson6/PairDemo.java
@@ -0,0 +1,15 @@
+package lessons.lesson6;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PairDemo {
+    public static void main(String[] args) {
+        List<Pair> pairList  = new ArrayList<>();
+
+        pairList.add(new Pair(20, "John", 3.5));
+
+
+
+    }
+}
